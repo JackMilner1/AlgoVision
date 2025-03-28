7a9e662e41ba2e24e5df5cf24948b86c6238d97e
Jack <117851346+JackMilner1@users.noreply.github.com>
2025-03-28 14:48:02 +0000

    graph additions

--- graph.py
+++ graph.py
@@ -8,9 +8,30 @@ class Graph():
     def addVertices(self):
         for i in range(self.width):
             for j in range(self.height):
-                newVertex = Vertex((i * self.width) + j)
+                index = (i * self.width) + j
+                newVertex = Vertex(index)
+                if index % self.width > 0:
+                    newVertex.addEdge(index - 1)
+
+                if index + 1 % self.width != 0:
+                    newVertex.addEdge(index + 1)
+
+                if index + (self.width - i) <= self.width * self.height - 1:
+                    newVertex.addEdge(index + (self.width - i))
+
+                if index - (self.width - i) >= 0:
+                    newVertex.addEdge(index - (self.width - i))
+
                 self.vertices = self.vertices + [newVertex]
 
+
+    def getVertices(self):
+        return self.vertices
+    
+    def printGraph(self):
+        for vertex in self.vertices:
+            vertex.printEdges()
+    
     def drawUI(self):
         pass
 
@@ -24,10 +45,25 @@ class Vertex():
         if not self.isWall:
             self.edges.append((self.id,whereToo,weight))
 
-    def removeEdge(self,whereToo,weight = 1):
-        if not self.isWall:
-            self.edges.remove((self.id,whereToo,weight))
+    def printEdges(self):
+        for edge in self.edges:
+            print(edge)
+
+    def getID(self):
+        return self.id
 
     def drawUI(self):
         pass
 
+    def convertToWall(self):
+        self.edges = []
+    
+    def revertWall(self):
+        self.isWall = False
+            
+
+newGraph = Graph(5,5)
+vertices = newGraph.getVertices()
+for vertex in vertices:
+    print(vertex.getID())
+    vertex.printEdges()
