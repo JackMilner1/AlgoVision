4fea0771521d006b14329d0198b52db4c3ba4b99
JackMilner1 <117851346+JackMilner1@users.noreply.github.com>
2025-04-10 21:16:34 +0100

    OOP implementation of tree visuals

--- GraphClasses.py
+++ GraphClasses.py
@@ -1,3 +1,5 @@
+import pygame
+
 class Graph():
     def __init__(self,width,height):
         self.vertices = []
@@ -108,4 +110,42 @@ class Vertex():
         self.visiting = False
         self.cameFrom = None
 
+class VertexGeometry(Vertex):
+    def __init__(self, id, x, y, isWall=False):
+        super().__init__(id, isWall)
+        self.x = x
+        self.y = y
 
+    def drawNode(self,screen):
+        x,y = self.x,self.y
+        nodeRect = pygame.Rect(x,y,100,100)
+        pygame.draw.rect(screen,(0,0,0),nodeRect,border_radius=100)
+        pygame.draw.rect(screen,(35, 35, 38),(x + 5,y + 5,90,90),border_radius=100,width=5)
+        self.draw_text(screen, str(self.id),nodeRect)
+
+    def drawEdges(self):
+        for edge in self.edges:
+            edge.drawEdge()
+            
+    def addEdge(self,screen, node2):
+        newEdge = EdgeGeometry(screen,(0,0,0),(self.x + 50 ,self.y + 50), (node2.x + 50,node2.y + 50),width=10)
+        self.edges.append(newEdge)
+
+    def draw_text(self,screen, text, button_rect):
+        text_color = (255, 255, 255)
+        font = pygame.font.Font(None, 24)
+        text_surface = font.render(text, True, text_color)
+        text_rect = text_surface.get_rect(center=button_rect.center)
+        screen.blit(text_surface, text_rect)
+
+class EdgeGeometry():
+    def __init__(self,screen,colour,xyFrom,xyToo,width = 10):
+        self.screen = screen
+        self.colour = colour
+        self.xyFrom = xyFrom
+        self.xyToo = xyToo
+        self.width = width
+    
+    def drawEdge(self):
+        pygame.draw.line(self.screen,self.colour,self.xyFrom, self.xyToo,width=10)
+    
--- TreeVisualiser.py
+++ TreeVisualiser.py
@@ -1,5 +1,6 @@
 import pygame
 import os 
+import GraphClasses
 
 pygame.init()
 
@@ -14,7 +15,20 @@ pygame.display.set_caption('Play')
 
 def start():
     running = True
-    nodes = [(400,500),(600,700),(600,300),(800,700),(800,300),(1000,700),(1000,300),(1200,500)]
+    node1 = GraphClasses.VertexGeometry(1,400,500)
+    node2 = GraphClasses.VertexGeometry(2,600,700)
+    node3 = GraphClasses.VertexGeometry(3,600,300)
+    node4 = GraphClasses.VertexGeometry(4,800,700)
+    node5 = GraphClasses.VertexGeometry(5,800,300)
+    node6 = GraphClasses.VertexGeometry(6,1000,700)
+    node7 = GraphClasses.VertexGeometry(7,1000,300)
+    node8 = GraphClasses.VertexGeometry(8,1200,500)
+    #nodes = [(400,500),(600,700),(600,300),(800,700),(800,300),(1000,700),(1000,300),(1200,500)]
+    node1.addEdge(screen,node2)
+    node2.addEdge(screen,node8)
+    node8.addEdge(screen,node3)
+    nodes = [node1,node2,node3,node4,node5,node6,node7,node8]
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -22,34 +36,16 @@ def start():
 
         screen.fill((43,43,55))
         
-        for node1 in nodes:
-            for node2 in nodes:
-                connectNodes(node1,node2)
+        for node in nodes:
+            node.drawEdges()
 
-        for i in range(len(nodes)):
-            node = nodes[i]
-            drawNode(node,i)
+        for node in nodes:
+            node.drawNode(screen)
 
         pygame.draw.rect(screen,(35, 35, 38),(0,0,SCREEN_WIDTH,SCREEN_HEIGHT*0.15))
         pygame.display.flip()
 
     pygame.quit()
 
-def drawNode(xy,text):
-    x,y = xy
-    nodeRect = pygame.Rect(x,y,100,100)
-    pygame.draw.rect(screen,(0,0,0),nodeRect,border_radius=100)
-    pygame.draw.rect(screen,(35, 35, 38),(x + 5,y + 5,90,90),border_radius=100,width=5)
-    draw_text(str(text),nodeRect)
 
-def connectNodes(node1,node2):
-    pygame.draw.line(screen,(0,0,0),(node1[0] + 50,node1[1] + 50), (node2[0] + 50,node2[1] + 50),width=10)
-
-def draw_text(text, button_rect):
-    text_color = (255, 255, 255)
-    font = pygame.font.Font(None, 24)
-    text_surface = font.render(text, True, text_color)
-    text_rect = text_surface.get_rect(center=button_rect.center)
-    screen.blit(text_surface, text_rect)
-    
 start()
