8b0b6ff9eaad1b01317b9913d2044615e4665f44
JackMilner1 <117851346+JackMilner1@users.noreply.github.com>
2025-04-22 23:46:34 +0100

    tree visuals reset and delete button

--- TreeVisualiser.py
+++ TreeVisualiser.py
@@ -20,14 +20,21 @@ def start():
 
     nodeFromSelected = None
 
-    screenClickArea = Button.Button(0,SCREEN_HEIGHT*0.15,SCREEN_WIDTH,SCREEN_HEIGHT*0.85,(43,43,55))
+    screenClickArea = Button.Button(0,SCREEN_HEIGHT*0.15,SCREEN_WIDTH*0.85,SCREEN_HEIGHT*0.85,(43,43,55))
+    resetButton = Button.Button(SCREEN_WIDTH * 0.9, SCREEN_HEIGHT * 0.5 + (95 * 2),90,90,(35, 35, 38),"Reset")
+    modeButton = Button.Button(SCREEN_WIDTH * 0.9, SCREEN_HEIGHT * 0.5 + 95,90,90,(35, 35, 38),"Mode: Add")
+    newestButton = Button.Button(SCREEN_WIDTH * 0.9, SCREEN_HEIGHT * 0.5,90,90,(35, 35, 38),"Run")
+    mode = "ADD"
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
+        screen.fill((43,43,55))
+
         if screenClickArea.drawButton(screen):
-            nodeFromSelected = manageScreenClick(nodes,nodeFromSelected)
+            nodeFromSelected = manageScreenClick(nodes,nodeFromSelected,mode)
+         
         
         for node in nodes:
             node.drawEdges()
@@ -35,18 +42,32 @@ def start():
         for node in nodes:
             node.drawNode(screen)
 
+        if resetButton.drawButton(screen):
+            nodes = []
+
+        if modeButton.drawButton(screen):
+            if mode == "ADD":
+                modeButton.changeText("Mode: Del")
+                mode = "DEL"
+            else:
+                modeButton.changeText("Mode: Add")
+                mode = "ADD"
+        
+        if newestButton.drawButton(screen):
+            pass
+
         pygame.draw.rect(screen,(35, 35, 38),(0,0,SCREEN_WIDTH,SCREEN_HEIGHT*0.15))
         pygame.display.flip() 
 
     pygame.quit()
 
-def manageScreenClick(nodes,nodeFromSelected):
+def manageScreenClick(nodes,nodeFromSelected,mode):
     coords = pygame.mouse.get_pos()
     x = coords[0] - 50
     y = coords[1] - 50
     canPlace = True
     for node in nodes:
-        if (x - 100 < node.x < x + 100) and (y - 100 < node.y < y + 100):
+        if (x - 100 < node.x < x + 100) and (y - 100 < node.y < y + 100) and mode == "ADD":
             canPlace = False
             if nodeFromSelected == None:
                 nodeFromSelected = node
@@ -55,12 +76,31 @@ def manageScreenClick(nodes,nodeFromSelected):
                 nodeFromSelected.addEdge(screen,node)
                 nodeFromSelected = None
             break
-                
-    if canPlace:
+        else:
+            if mode == "DEL" and ((x - 100 < node.x < x + 100) and (y - 100 < node.y < y + 100)):
+                print("in")
+                target = (node.x,node.y)
+                print(f"coords: {(node.x,node.y)}" )
+                nodes.remove(node)
+                cleanup(nodes,target)
+  
+    if canPlace and mode == "ADD":
         nodeFromSelected = None
         newNode = GraphClasses.VertexGeometry(len(nodes) + 1,x,y)
         nodes.append(newNode)
 
     return nodeFromSelected
+
+def cleanup(nodes,target):
+    for node in nodes:
+        for i in node.edges:
+            print(node.id)
+            xToo = i.xyToo[0] - 50
+            yToo = i.xyToo[1] - 50
+            print((xToo,yToo))
+            if (xToo,yToo) == target:
+                print("connection")
+                node.edges.remove(i)
+
 
 start()
